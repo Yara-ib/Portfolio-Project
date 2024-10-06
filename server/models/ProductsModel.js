75c0d7cf1b595812b85879139deb3a4b9f4d557f
Yara-ib <yr.ap.23@gmail.com>
2024-10-06 04:53:07 +0300

    Create Product Model

---
+++ server/models/ProductsModel.js
@@ -0,0 +1,50 @@
+import { model, Schema } from 'mongoose';
+
+const ProductsSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    countryOfOrigin: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = model('Product', ProductsSchema);
+export default Product;
