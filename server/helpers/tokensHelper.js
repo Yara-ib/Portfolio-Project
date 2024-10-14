937c5903855c6f50f24f6813d5b54a5db2091048
Yara-ib <yr.ap.23@gmail.com>
2024-10-14 22:13:38 +0300

    Update: Adding Token verification through the header from requests & logging it to the system for monitoring

--- server/helpers/tokensHelper.js
+++ server/helpers/tokensHelper.js
@@ -4,3 +4,25 @@ export const getNewToken = (id) => {
   return jwt.sign({ id }, process.env.JWToken, { expiresIn: '7d' });
 };
 
+export const getTokenFromHeader = (req) => {
+  if (req?.headers?.authorization) {
+    const token = req.headers.authorization.split(' ')[1];
+    return token;
+  } else {
+    console.log('Someone tried to access Profile page, but token wasn\'t found!');
+    return 'Token not found!';
+  }
+};
+
+export const verifyTokenToGetID = (token) => {
+  return jwt.verify(token, process.env.JWToken, (error, decodedTokenData) => {
+    if (!error) {
+      return decodedTokenData;
+    } else {
+      return false;
+    }
+
+
+monitoring
+  });
+};
