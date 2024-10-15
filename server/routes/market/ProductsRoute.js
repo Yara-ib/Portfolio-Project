616ebd237b7868e4b81b9cd14992abace5385c11
Yara-ib <yr.ap.23@gmail.com>
2024-10-16 01:53:23 +0300

    Add: Getting One Product Route by Id, can accessed by anyone

--- server/routes/market/ProductsRoute.js
+++ server/routes/market/ProductsRoute.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { addProduct } from '../../controllers/productsController.js';
+import {
+  addProduct,
+  getProduct,
+} from '../../controllers/productsController.js';
 import { adminAccess } from '../../middlewares/adminAccess.js';
 import { checkAccess } from '../../middlewares/checkAccess.js';
 
 export const productRoutes = Router();
 
 productRoutes.post('/add', checkAccess, adminAccess, addProduct);
+productRoutes.get('/:id', getProduct);
