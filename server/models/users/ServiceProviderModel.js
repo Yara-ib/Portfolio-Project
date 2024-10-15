59dd5386cebbbdfdc6b800666529167a3739c05e
Yara-ib <yr.ap.23@gmail.com>
2024-10-15 20:59:21 +0300

    Add: Service Providers Model with reference to Services which'll be added

---
+++ server/models/users/ServiceProviderModel.js
@@ -0,0 +1,58 @@
+import { Schema, model } from 'mongoose';
+
+const ServiceProviderSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 4,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    biography: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: '../assets/images/profilePic.png',
+    },
+    servicesOffered: [
+      {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Service',
+      },
+    ],
+    adminOrNot: {
+      type: Boolean,
+      default: false,
+    },
+    bannedOrNot: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ServiceProvider = model('ServiceProvider', ServiceProviderSchema);
+export default ServiceProvider;
