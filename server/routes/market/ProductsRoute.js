31420ab2d16c71feb2ee6ef964d73b235ac131ca
Yara-ib <yr.ap.23@gmail.com>
2024-10-16 01:36:55 +0300

    Security: Adding further Checks to access CRUD operations for the products

--- server/routes/market/ProductsRoute.js
+++ server/routes/market/ProductsRoute.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { addProduct } from '../../controllers/productsController.js';
+import { adminAccess } from '../../middlewares/adminAccess.js';
+import { checkAccess } from '../../middlewares/checkAccess.js';
 
 export const productRoutes = Router();
 
-productRoutes.post('/add', addProduct);
+productRoutes.post('/add', checkAccess, adminAccess, addProduct);
