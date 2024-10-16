a93632430a9c1694c121059f63f441625360716b
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 02:55:35 +0300

    Add: Route for getting Profile Page for Bloggers after sign in

--- server/routes/blog/bloggersRoutes.js
+++ server/routes/blog/bloggersRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import {
+  getProfileBlogger,
   signInBlogger,
   signUpBlogger,
 } from '../../controllers/blog/bloggersController.js';
+import { checkAccessBlogger } from '../../middlewares/checkAccess.js';
 
 export const bloggersRoutes = Router();
 bloggersRoutes.post('/newblogger', signUpBlogger);
 bloggersRoutes.post('/logblogger', signInBlogger);
+
+bloggersRoutes.get('/profile', checkAccessBlogger, getProfileBlogger);
