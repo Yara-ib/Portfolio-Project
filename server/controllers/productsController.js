56cf730e1409810ea5b6f39197896df9be9ca84c
Yara-ib <yr.ap.23@gmail.com>
2024-10-16 19:57:04 +0300

    Add: Updating Product functionality using Product Id

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -108,3 +108,56 @@ export const deleteProduct = async (req, res) => {
     }
   }
 };
+
+export const updateProduct = async (req, res) => {
+  const {
+    productName,
+    mainCategory,
+    subCategory,
+    brand,
+    images,
+    color,
+    price,
+    description,
+    countryOfOrigin,
+  } = req.body;
+
+  if (req.params.id) {
+    if (!checkValidId(req)) {
+      return res.status(400).json({
+        message: 'Please enter a valid id',
+      });
+    } else {
+      const productToUpdate = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+          productName,
+          mainCategory,
+          subCategory,
+          brand,
+          images,
+          color,
+          price,
+          description,
+          countryOfOrigin,
+        },
+        {
+          // To make sure the returned value is updated
+          // because default for findByIdAndUpdate returns the old document
+          new: true,
+        }
+      );
+      if (productToUpdate) {
+        console.log(`Product was successfully updated!`);
+        return res.status(200).json({
+          message: 'Product was successfully updated!',
+          productToUpdate,
+        });
+      } else {
+        return res.status(404).json({
+          message: 'No product matches that id!',
+        });
+      }
+    }
+  }
+};
