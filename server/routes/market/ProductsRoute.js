8771b98e433ac60131540d95d4384b54be52c9a3
Yara-ib <yr.ap.23@gmail.com>
2024-10-16 20:18:28 +0300

    Add: Route to handle getting all Products, accessed by anyone

--- server/routes/market/ProductsRoute.js
+++ server/routes/market/ProductsRoute.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   addProduct,
   deleteProduct,
+  getAllProducts,
   getProduct,
   updateProduct,
 } from '../../controllers/productsController.js';
@@ -10,6 +11,7 @@ import { checkAccess } from '../../middlewares/checkAccess.js';
 
 export const productRoutes = Router();
 
+productRoutes.get('/', getAllProducts);
 productRoutes.get('/:id', getProduct);
 productRoutes.post('/add', checkAccess, adminAccess, addProduct);
 productRoutes.delete('/delete/:id', checkAccess, adminAccess, deleteProduct);
