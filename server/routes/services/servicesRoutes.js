692b55522600700297c315c09e61aada18ba1cce
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 09:39:24 +0300

    Add: Route of handling deleting certain Service by its Id, but only accessed for Admins

--- server/routes/services/servicesRoutes.js
+++ server/routes/services/servicesRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import {
   addService,
+  deleteService,
   getAllService,
   getService,
   updateService,
 } from '../../controllers/services/servicesController';
+import { adminAccess } from '../../middlewares/adminAccess.js';
+import { checkAccess } from '../../middlewares/checkAccess.js';
 import { checkAccessProvider } from '../../middlewares/checkAccess.js';
 
 export const servicesRoutes = Router();
@@ -15,3 +18,5 @@ servicesRoutes.get('/:id', getService);
 servicesRoutes.post('/add', checkAccessProvider, addService);
 servicesRoutes.put('/update/:id', checkAccessProvider, updateService);
 
+// Only Admins for now are able to delete Services
+servicesRoutes.delete('/delete/:id', checkAccess, adminAccess, deleteService);
