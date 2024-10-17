f8d4c89c3d286caed4c8690f0310527152c2a4f3
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 05:09:13 +0300

    Add: Check Access By Token For Service Providers

--- server/middlewares/checkAccess.js
+++ server/middlewares/checkAccess.js
@@ -4,6 +4,7 @@ import {
   verifyTokenToGetID,
 } from '../helpers/tokensHelper.js';
 import Blogger from '../models/users/BloggersModel.js';
+import ServiceProvider from '../models/users/ServiceProviderModel.js';
 import User from '../models/users/UsersModel.js';
 
 export const checkAccess = async (req, res, next) => {
@@ -31,6 +32,22 @@ export const checkAccessBlogger = async (req, res, next) => {
     const BloggerById = await Blogger.findById(access.id);
     console.log(
       `${BloggerById.username}: (${BloggerById.username}) granted access as a blogger`
+    );
+    next();
+  } else {
+    errorHelper(req, res, 'Access Denied, please try logging in again', 403);
+  }
+};
+
+export const checkAccessProvider = async (req, res, next) => {
+  const token = getTokenFromHeader(req);
+  const access = verifyTokenToGetID(token);
+
+  if (access) {
+    req.authorizedId = access.id;
+    const ProviderById = await ServiceProvider.findById(access.id);
+    console.log(
+      `${ProviderById.username}: (${ProviderById.username}) granted access as a service provider`
     );
     next();
   } else {
