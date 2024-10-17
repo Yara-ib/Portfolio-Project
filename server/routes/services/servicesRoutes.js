e197ac0f6c1d3af82d3b70393dfc8c8af9c01db8
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 09:20:57 +0300

    Add: Get Routes for getting Services & accessed by anyone

--- server/routes/services/servicesRoutes.js
+++ server/routes/services/servicesRoutes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import {
   addService,
+  getAllService,
+  getService,
   updateService,
 } from '../../controllers/services/servicesController';
 import { checkAccessProvider } from '../../middlewares/checkAccess.js';
 
 export const servicesRoutes = Router();
+
+servicesRoutes.get('/', getAllService);
+servicesRoutes.get('/:id', getService);
 
 servicesRoutes.post('/add', checkAccessProvider, addService);
 servicesRoutes.put('/update', checkAccessProvider, updateService);
