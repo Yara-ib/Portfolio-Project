d242ec851fa0bca8516f413d1752b21e7774925c
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 06:46:20 +0300

    Add: Route for Getting Posts by Id && it's accessed by anyone

--- server/routes/blog/PostsRoutes.js
+++ server/routes/blog/PostsRoutes.js
@@ -3,9 +3,11 @@ import { checkAccessBlogger } from '../../middlewares/checkAccess.js';
 import {
   addPost,
   getAllPosts,
+  getPost,
 } from '../../controllers/blog/postsController.js';
 
 export const postsRoutes = Router();
 
 postsRoutes.post('/addPost', checkAccessBlogger, addPost);
 postsRoutes.get('/posts', getAllPosts);
+postsRoutes.get('/posts/:id', getPost);
