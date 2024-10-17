4bf1612a45042d4c0ddc8f1a78fa52a11d7fa3a4
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 06:42:09 +0300

    Add: The Route to GET all Posts added, accessed by anyone

--- server/routes/blog/PostsRoutes.js
+++ server/routes/blog/PostsRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { checkAccessBlogger } from '../../middlewares/checkAccess.js';
-import { addPost } from '../../controllers/blog/postsController.js';
+import {
+  addPost,
+  getAllPosts,
+} from '../../controllers/blog/postsController.js';
 
 export const postsRoutes = Router();
 
 postsRoutes.post('/addPost', checkAccessBlogger, addPost);
+postsRoutes.get('/posts', getAllPosts);
