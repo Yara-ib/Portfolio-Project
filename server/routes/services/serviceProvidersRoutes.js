89ba8336a43f55ac48182d2b5028e1f6215d9b36
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 05:19:43 +0300

    Add: Protected Route to get Profile Page for Service Providers

--- server/routes/services/serviceProvidersRoutes.js
+++ server/routes/services/serviceProvidersRoutes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
-import { signInProvider, signUpProvider } from '../../controllers/services/serviceProvidersController.js';
+import {
+  getProfileProvider,
+  signInProvider,
+  signUpProvider,
+} from '../../controllers/services/serviceProvidersController.js';
+import { checkAccessProvider } from '../../middlewares/checkAccess.js';
 
 export const serviceProvidersRoutes = Router();
 
 serviceProvidersRoutes.post('/newProvider', signUpProvider);
 serviceProvidersRoutes.post('/logProvider', signInProvider);
+
+serviceProvidersRoutes.get(
+  '/profileProvider',
+  checkAccessProvider,
+  getProfileProvider
+);
