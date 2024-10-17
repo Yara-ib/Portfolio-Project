0f97aa88167dadea7c57eb680cad3e136a782834
Yara-ib <yr.ap.23@gmail.com>
2024-10-17 05:10:11 +0300

    Add: Getting all Routes related to Services Section

--- server/middlewares/handlingMainRoutes.js
+++ server/middlewares/handlingMainRoutes.js
@@ -4,15 +4,20 @@ import { authorizationRoutes } from '../routes/users/authorizationRoutes.js';
 import { errorHelper } from '../helpers/errorHelper.js';
 import { productRoutes } from '../routes/market/ProductsRoute.js';
 import { bloggersRoutes } from '../routes/blog/bloggersRoutes.js';
+import { serviceProvidersRoutes } from '../routes/services/serviceProvidersRoutes.js';
 
 export const mainRoutes = Router();
 
 // Getting all routes
+// Users & Customers Routes
 mainRoutes.use('/users', authenticationRoutes);
 mainRoutes.use('/users', authorizationRoutes);
 
-// Product Routes
+// Products Routes
 mainRoutes.use('/products', productRoutes);
+
+// Services Routes
+mainRoutes.use('/services', serviceProvidersRoutes);
 
 // Blog Routes
 mainRoutes.use('/blog', bloggersRoutes);
