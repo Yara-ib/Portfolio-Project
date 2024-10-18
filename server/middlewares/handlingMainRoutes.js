d8614a2955933f309bf38375f25074b95f535969
Yara-ib <yr.ap.23@gmail.com>
2024-10-18 21:40:34 +0300

    Add: Routes for Orders in Market Section & organizing the code a bit

--- server/middlewares/handlingMainRoutes.js
+++ server/middlewares/handlingMainRoutes.js
@@ -7,25 +7,37 @@ import { postsRoutes } from '../routes/blog/PostsRoutes.js';
 import { productRoutes } from '../routes/market/ProductsRoute.js';
 import { serviceProvidersRoutes } from '../routes/services/serviceProvidersRoutes.js';
 import { servicesRoutes } from '../routes/services/servicesRoutes.js';
+import { ordersRoutes } from '../routes/market/OrdersRoutes.js';
 
 export const mainRoutes = Router();
 
-// Getting all routes
-// Users & Customers Routes
+// Getting all Routes
+
+// ~ General Users, Customers and Admins
+// Users Sign Up & Sign In Routes:
 mainRoutes.use('/users', authenticationRoutes);
+// Restricted Areas: accessible for Users only
 mainRoutes.use('/users', authorizationRoutes);
 
+// ~ Market Section Routes::
 // Products Routes
 mainRoutes.use('/products', productRoutes);
+// Orders Routes
+mainRoutes.use('/orders', ordersRoutes);
 
+// ~ Services Section Routes::
 // Services Routes
-mainRoutes.use('/services', serviceProvidersRoutes);
 mainRoutes.use('/services', servicesRoutes);
+// Service Providers Routes
+mainRoutes.use('/services', serviceProvidersRoutes);
 
-// Blog Routes
-mainRoutes.use('/blog', bloggersRoutes);
+// ~ Blog Section Routes::
+// Posts Routes
 mainRoutes.use('/blog/posts', postsRoutes);
+// Bloggers Routes
+mainRoutes.use('/blog', bloggersRoutes);
 
+// ~ Checking Any wrong Path
 // Route NOT Found "Last To check"
 mainRoutes.use((req, res) => {
   errorHelper(req, res, 'Page Not Found', 404);
