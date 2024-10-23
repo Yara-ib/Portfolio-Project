2cc7dbb5f70e45943eca49a7b1785d4640c471bf
Yara-ib <yr.ap.23@gmail.com>
2024-10-24 02:48:09 +0300

    Add: Routes for handling Updating & Deleting Account for Service Providers

--- server/routes/services/serviceProvidersRoutes.js
+++ server/routes/services/serviceProvidersRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import {
+  deleteServiceProvider,
   getProfileProvider,
   signInProvider,
   signUpProvider,
+  updateServiceProvider,
 } from '../../controllers/services/serviceProvidersController.js';
 import { checkAccessProvider } from '../../middlewares/checkAccess.js';
 
@@ -11,8 +13,21 @@ export const serviceProvidersRoutes = Router();
 serviceProvidersRoutes.post('/newProvider', signUpProvider);
 serviceProvidersRoutes.post('/logProvider', signInProvider);
 
+// Profile & Account Management
 serviceProvidersRoutes.get(
   '/profileProvider',
   checkAccessProvider,
   getProfileProvider
+);
+
+serviceProvidersRoutes.put(
+  '/profile/update',
+  checkAccessProvider,
+  updateServiceProvider
+);
+serviceProvidersRoutes.delete(
+  '/profile/delete',
+  checkAccessProvider,
+  adminAccess,
+  deleteServiceProvider
 );
