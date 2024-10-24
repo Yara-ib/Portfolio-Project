da7729474acdc7f48d222e9e027b3505e63648c5
Yara-ib <yr.ap.23@gmail.com>
2024-10-24 04:20:27 +0300

    Add: Routes for Getting All Orders OR by Id

--- server/routes/market/OrdersRoutes.js
+++ server/routes/market/OrdersRoutes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import { addOrder } from '../../controllers/market/ordersController.js';
+import {
+  addOrder,
+  getAllOrder,
+  getOrder,
+} from '../../controllers/market/ordersController.js';
 import { checkAccess } from '../../middlewares/checkAccess.js';
 
 export const ordersRoutes = Router();
 
 ordersRoutes.post('/add', checkAccess, addOrder);
+ordersRoutes.get('/', checkAccess, getAllOrder);
+ordersRoutes.get('/:id', checkAccess, getOrder);
