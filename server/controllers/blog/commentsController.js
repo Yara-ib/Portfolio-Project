4adbba08376d8a3decc274307469f05e109e6db7
Yara-ib <yr.ap.23@gmail.com>
2024-10-25 05:55:13 +0300

    Fix: Adding Check if post that comment added to wasn't deleted from database

--- server/controllers/blog/commentsController.js
+++ server/controllers/blog/commentsController.js
@@ -1,24 +1,30 @@
 import { errorHelper } from '../../helpers/errorHelper.js';
 import Comment from '../../models/blog/CommentsModel.js';
+import Post from '../../models/blog/PostsModel.js';
 
 export const addComment = async (req, res) => {
-  const { title, commentItself } = req.body;
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    const { title, commentItself } = req.body;
 
-  if (!title || !commentItself) {
-    errorHelper(req, res, 'Please fill in all the required fields', 400);
+    if (!title || !commentItself) {
+      errorHelper(req, res, 'Please fill in all the required fields', 400);
+    }
+
+    const newComment = new Comment({
+      post: req.params.id,
+      title,
+      commentItself,
+      user: req.authorizedId,
+    });
+    await newComment.save();
+    console.log('New comment been added!');
+
+    res.status(201).json({
+      message: 'Your comment been added.',
+      comment: newComment,
+    });
+  } else {
+    errorHelper(req, res, 'No Post Found', 400);
   }
-
-  const newComment = new Comment({
-    post: req.params.id,
-    title,
-    commentItself,
-    user: req.authorizedId,
-  });
-  await newComment.save();
-  console.log('New comment been added!');
-
-  res.status(201).json({
-    message: 'Your comment been added.',
-    comment: newComment,
-  });
 };
